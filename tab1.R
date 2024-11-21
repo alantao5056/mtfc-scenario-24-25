b84847b444648cc74b48a1b961912f7ccd6cb732
alantao5056 <alantaolr@outlook.com>
2024-11-20 21:06:07 -0500

    q17 plots

--- tab1.R
+++ tab1.R
@@ -87,3 +87,62 @@ q9 |>
        subtitle = "Metformin is the most expensive")
 q9
 
+# q17
+q17 =
+  tab1 |>
+  mutate(total_claim = ifelse(is.na(insulin_claims), 0, insulin_claims) +
+                       ifelse(is.na(metformin_claims), 0, metformin_claims),
+         total_cost = annual_medical_costs + annual_pharmacy_costs) |>
+  select(total_claim, age, a1c_score, total_cost)
+
+# claim
+fit = lm(total_cost~total_claim, q17)
+xCoef = fit[[1]][[2]]
+constant = fit[[1]][[1]]
+r2 = summary(fit)$r.squared
+
+
+q17 |>
+  ggplot(aes(x = total_claim, y = total_cost)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm", se = FALSE) +
+  annotate("text", x=40, y=12500, label="R^2=0.045", size = 5) +
+  scale_y_continuous(label = scales::dollar_format()) +
+  theme_bw() +
+  labs(title = "Total Claim and Total Cost",
+       subtitle = "R^2 is too small, seems like there's no correlation",
+       x = "Total Claim",
+       y = "Total Cost")
+
+# age
+fit2 = lm(total_cost~age, q17)
+r2 = summary(fit2)$r.squared
+
+q17 |>
+  ggplot(aes(x = age, y = total_cost)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm", se = FALSE) +
+  annotate("text", x=43, y=90000, label="R^2=0.005", size = 5) +
+  scale_y_continuous(label = scales::dollar_format()) +
+  theme_bw() +
+  labs(title = "Age and Total Cost",
+       subtitle = "R^2 is too small, seems like there's no correlation",
+       x = "Age",
+       y = "Total Cost")
+
+# a1c
+
+fit3 = lm(total_cost~a1c_score, q17)
+r3 = summary(fit3)$r.squared
+
+q17 |>
+  ggplot(aes(x = a1c_score, y = total_cost)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm", se = FALSE) +
+  annotate("text", x=14, y=90000, label="R^2=0.0001", size = 5) +
+  scale_y_continuous(label = scales::dollar_format()) +
+  theme_bw() +
+  labs(title = "A1C Score and Total Cost",
+       subtitle = "Line is flat, R^2 is too small",
+       x = "A1C Score",
+       y = "Total Cost")
